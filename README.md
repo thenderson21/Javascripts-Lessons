a5c0cc23de3dfcc8408db818273b428893510b34
Todd Henderson <thenderson@spe.org>
2013-07-10 22:54:16 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+##Installation
+Install NodeJS
---
+++ app.js
@@ -0,0 +1,44 @@
+/*
+ * Module dependencies
+ */
+var express = require('express'),
+	stylus = require('stylus'),
+	nib = require('nib');
+
+	
+// !Express Server
+var app = express();
+
+function compile(str, path) {
+  return stylus(str)
+    .set('filename', path)
+    .use(nib())
+};
+
+//Set up Jade as 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
+app.use(express.logger('dev'));
+app.use(stylus.middleware({ 
+  	src: __dirname + '/public',
+  	compile: compile
+  })
+);
+app.use(express.static(__dirname + '/public'));
+
+
+// App url directives
+app.get('/', function (req, res) {
+  res.render('index',{
+		title : 'Javascript Lessons'
+	});
+});
+
+app.get('/:page', function (req, res){
+	res.render(req.params.page,{
+		title : 'Javascript Lessons'
+	});
+});
+
+app.listen(3000);
